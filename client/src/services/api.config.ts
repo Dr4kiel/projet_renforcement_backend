f92a16b8d1f7b7b8e902131694787247b58fc073
Cyprideux53000 <cyprien.gaudin@supdevinci-edu.fr>
2026-02-06 16:38:03 +0100

    chore(client): correct call API

--- client/src/services/api.config.ts
+++ client/src/services/api.config.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5050';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 const API_VERSION = 'v1';
 
